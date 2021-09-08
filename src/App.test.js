d8f446c1b1b90e3f6470afb55ed11cc0304586f5
unknown <AlyssaNWilson@gmail.com>
2021-09-08 14:23:32 -0500

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+/* too scared to delete this right away
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+*/
