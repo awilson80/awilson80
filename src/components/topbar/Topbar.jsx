bc856424f4c65267d57c0747697b4615487bc3e1
unknown <AlyssaNWilson@gmail.com>
2021-09-15 20:04:59 -0500

    Adds topbar, menu functionality

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,42 @@
 import "./topbar.scss"
+import { DonutLargeRounded, MailRounded} from "@material-ui/icons"
 
-export default function Topbar() {
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+
+            <div className="wrapper">
+
+                {/* Write text here for left side of topbar */}
+                <div className="left">
+                    <a href="#intro" className="logo">a.w.</a>
+                    
+                    <div className="itemContainer">
+                        <DonutLargeRounded className="icon" />
+                        <span>(405)659-4996</span>
+                    </div>
+                    
+                    <div className="itemContainer">
+                        <MailRounded className="icon"/>
+                        <span>AlyssaNWilson308@gmail.com</span>   
+                    </div>
+
+                </div>
+
+                {/* Write text between the divs for centered text */}
+
+                {/* Write text here for right side of topbar */}
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
